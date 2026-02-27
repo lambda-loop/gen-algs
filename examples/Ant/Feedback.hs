f186cbd33929c471d903a2852a3338face0a6397
JJoaoll <joaoduos@gmail.com>
2026-02-27 10:26:41 -0300

    checkpoint Feedback

---
+++ examples/Ant/Feedback.hs
@@ -0,0 +1,113 @@
+
+module Ant.Feedback where
+import qualified Data.Vector.Strict as Vec
+import qualified Ant.World.State as World
+import Graphics.Gloss
+import Ant.World.Screen
+import Ant.Dna
+
+import qualified System.Random.MWC as MWC
+import Graphics.Gloss.Data.ViewPort (ViewPort)
+import Ant.World.Flow
+
+import Control.Concurrent.STM
+-- import Genome
+
+data Model = Model 
+  { sync   :: TVar (Vec.Vector Mind)
+  , actual :: Vec.Vector World.State
+  }
+
+mkModel :: TVar (Vec.Vector Mind) -> Model
+mkModel tVec = Model 
+  { sync   = tVec
+  , actual = Vec.empty
+  }
+
+statePainter :: Vec.Vector World.State -> IO Picture 
+statePainter = pictureStates 
+
+painter :: Model -> IO Picture
+painter = statePainter . actual
+
+updater :: ViewPort -> Float -> Model -> IO Model
+updater _ _ model = do 
+  let v = actual model
+  if stillRunning v then do
+    v' <- Vec.forM v step
+    pure model { actual = v'}
+  else do
+    minds  <- readTVarIO (sync model)
+    states <- Vec.forM minds setupMind
+    pure model { actual = states }
+
+  where 
+    stillRunning = Vec.any ((== World.Running) . World.status) 
+
+-- -- TODO: additional informqation [Fen Mind Integer] 
+-- stageGames :: StateT (Vec.Vector World.State) IO ()
+-- stageGames = do
+--   states <- get
+--
+--   -- WARNING: slow
+--   states'<- Vec.forM states $ \s -> do
+--     if World.status s == World.Over 
+--         then pure s
+--       else liftIO (step s)
+--       
+--   put states'
+--   pics <- liftIO . pictureStates $ Vec.toList states'
+--   undefined
+--
+--   -- WARNING: dammmm slow
+--   when (Vec.any ((== World.Running) . World.status) states') 
+--     stageGames 
+--
+
+pictureStates :: Vec.Vector World.State -> IO Picture
+pictureStates states = do
+  pics <- Vec.forM states $ \state -> do
+    modelPainter state
+
+  -- TODO: convert to a state based version 
+  -- with to improve perfm with vecs
+  let pics' = lazy (Vec.toList pics) 0
+  pure $ pictures pics'
+  where -- TODO: refactor to use tail recursion (good enough)
+    lazy [] _ = []
+    lazy (p:ps) acc = 
+      let acc' = acc + 31 in
+      translate acc' 0 p : lazy ps acc'
+--
+--       
+--       
+--       
+--
+--
+--     
+--
+-- -- modelPainter :: World.State -> IO Picture
+-- -- modelPainter World.State {..} = do
+-- --   let blocks' = putTile white <$> blocks 
+-- --       foods'  = putTile blue  <$> foods
+-- --       ant     = putTile red    $  World.ant_pos player 
+-- --
+-- --   pure . pictures $
+-- --     Vec.toList blocks' 
+-- --     ++ Vec.toList foods'
+-- --     ++ [ant]
+-- --   where 
+-- --     putTile c (Pos2D (x, y)) =
+-- --       color c . translate (x'*tileSize) (y'*tileSize)
+-- --         $ rectangleSolid tileSize tileSize
+-- --       where x' = fromIntegral x
+-- --             y' = fromIntegral y
+-- --   
+-- --
+-- --
+-- --
+
+setupMind :: Mind -> IO World.State
+setupMind mind = do
+  gen <- MWC.createSystemRandom
+  initialState gen mind
